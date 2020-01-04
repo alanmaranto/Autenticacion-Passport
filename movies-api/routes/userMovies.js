b47cbfb09b642ab41aa040961c3ffbf2ac1bc9e9
alan <alanarostegui@hotmail.com>
2020-01-04 16:06:09 -0600

    agregando la coleccion de peliculas de usuario(obtenerlas)

---
+++ movies-api/routes/userMovies.js
@@ -0,0 +1,33 @@
+const express = require('express');
+
+const { UserMoviesService } = require('../services/userMovies');
+const validationHandler = require('../utils/middleware/validationHandler');
+
+const { movieIdSchema } = require('../utils/schemas/movies');
+const { userIdSchema } = require('../utils/schemas/users');
+const { userMovieIdSchema } = require('../utils/schemas/userMovies');
+
+const userMoviesApi = app => {
+  const router = express.Router();
+  app.user('/api/user-movies', router);
+
+  const userMoviesService = new UserMoviesService();
+
+  router.get(
+    '/',
+    validationHandler({ userId: userIdSchema }, 'query'),
+    async (req, res, next) => {
+      const { userId } = req.query;
+
+      try {
+        const userMovies = await userMoviesService.getUserMovies({ userId });
+        res.status(200).json({
+          data: userMovies,
+          message: 'user movies listed'
+        });
+      } catch (error) {
+        next(error);
+      }
+    }
+  );
+};
