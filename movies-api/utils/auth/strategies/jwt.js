cd4f65df770cd85e4b22745b9cf26bd6528b2fa5
alan <alanarostegui@hotmail.com>
2020-01-10 16:53:41 -0600

    implementacion de strategy y extractjwt con passport.js

---
+++ movies-api/utils/auth/strategies/jwt.js
@@ -0,0 +1,30 @@
+const passport = require('passport-http')
+const { Strategy, ExtractJwt } = require('passport-jwt');
+const boom = require('@hapi/boom');
+
+const UsersService = require('../../../services/users'); // De aqui buscaremos el usuario por el email que extraigamos del JWT
+const { config } = require('../../../config'); // De aqui le haremos saber a la estrategia con que secret fue firmado el JWT y sepa si es valido
+
+passport.use(
+    new Strategy({
+        secretOrKey: config.authJwtSecret, // recibe el jwtsecret
+        jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken() //especificamos de donde sacamos el jwt, en este caso lo enviaremos en las peticiones en el token
+    },
+    // en la funcion cb recibimos token y error
+    async (tokenPayload, callback) => {
+        const usersService = new UsersService();
+
+        try {
+            const user = await usersService.getUser({ email: tokenPayload.email });
+
+            if (!user) {
+                return callback(boom.unauthorized(), false);
+            }
+
+            delete user.password;
+            callback(null, {...user, scopes: tokenPayload.scopes })
+        } catch (error) {
+            return callback(error);
+        }
+    })
+);
